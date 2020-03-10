332ef1d31fdccf83bbf1c1378f5480d50045c276
Wesley Guerra <wesleyguerra9@gmail.com>
2020-03-10 14:20:32 -0300

    Atualizando

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+$(function () {
+    $('.links a').click(function () {
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({ 'scrollTop': offSetTop });
+
+        return false;
+    })
+
+
+
+    // $('.bn1-texto a').click(function () {
+    //     var href = $(this).attr('href');
+    //     var offSetTop = $(href).offset().top;
+    //     $('html,body').animate({ 'scrollTop': offSetTop });
+
+    //     return false;
+    // })
+
+
+})
