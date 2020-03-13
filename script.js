993eb50f42ee74e87705d3b9d3b090347a64ac6b
Wesley Guerra <wesleyguerra9@gmail.com>
2020-03-13 11:23:00 -0300

    Atualizando

--- script.js
+++ script.js
@@ -20,8 +20,35 @@ $(function () {
 })
 
 
+
 /* funcionalidade no menu */
 const buttonMenu = document.querySelector('.menu-opener')
 buttonMenu.addEventListener('click',()=>{
     document.querySelector('.links').classList.toggle('oculte')
 })
+
+
+
+// /* animação */
+const target = document.querySelectorAll('[data-anime]')
+const animationClass =  'animate';
+
+function animeScroll(){
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
+    target.forEach(function(element){
+        if ((windowTop) > element.offsetTop ){
+            element.classList.add(animationClass)
+        }else{
+            element.classList.remove(animationClass)
+        }
+            
+    })
+}
+animeScroll();
+if(target.length){
+    window.addEventListener('scroll', function () {
+        animeScroll();
+    })
+}
+
+
