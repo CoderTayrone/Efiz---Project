532ebc529304d8de452219cb2e772ede30b21f35
Wesley Guerra <wesleyguerra9@gmail.com>
2020-03-14 07:32:18 -0300

    Atualizando

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ $(function () {
 
 
 
-    // $('.bn1-texto a').click(function () {
-    //     var href = $(this).attr('href');
-    //     var offSetTop = $(href).offset().top;
-    //     $('html,body').animate({ 'scrollTop': offSetTop });
+    $('.button a').click(function () {
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+        $('html,body').animate({ 'scrollTop': offSetTop });
 
-    //     return false;
-    // })
+        return false;
+    })
 })
 
 
