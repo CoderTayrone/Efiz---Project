b854ae7d30d4490b6555e84028c5fdd162ad6771
Wesley Guerra <wesleyguerra9@gmail.com>
2020-03-15 10:19:07 -0300

    Atualizando

--- script.js
+++ script.js
@@ -43,12 +43,27 @@ function animeScroll(){
         }
             
     })
-}
+};
 animeScroll();
 if(target.length){
     window.addEventListener('scroll', function () {
         animeScroll();
     })
-}
+};
 
+
+/* botão iframe */
+const btn = document.querySelector('.btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+btn.addEventListener("click", function () {
+    modalOverlay.classList.remove('hide');
+    modalOverlay.querySelector("iframe").src =`https://www.youtube.com/embed/n161-SxGDPQ`; 
+
+
+});
+
+document.querySelector('.close-modal').addEventListener("click", function () {
+    modalOverlay.classList.add("hide");
+    modalOverlay.querySelector("iframe").src = ``;
+})
 
